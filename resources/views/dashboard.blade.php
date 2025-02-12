95cabbc826d830b779795ff8d238a46118c7545b
pistorello <91835285+pistorello@users.noreply.github.com>
2025-02-12 19:26:59 -0300

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,20 +11,63 @@
     <h1>Dashboard</h1> 
 
     <div class="card">
-      <h3>Total Inventories:</h3>
-      <h2>{{ count($inventories) }}</h2>
+      <div class="tab-content">  
+        <div id="tab-total-inventory">
+          <h3>Total Inventories:</h3>
+          <h2>{{ count($inventories) }}</h2>
+        </div>  
+
+        <div id="tab-total-users" style="display: none">
+          <h3>Total Users:</h3>
+          <h2>{{ count($users) }}</h2>
+        </div>
+      </div>
+      <div class="tab-dots">
+        <span class="dot active" onclick="switch_tab(1)"></span> 
+        <span class="dot" onclick="switch_tab(2)"/>
+      </div>
     </div>
 
-    <div>
-      <h3>Quick Access</h3>
-      <a href="{{ route('show_inventory_form') }}">
-        <button>Add Inventory   </button>
-      </a>
-      <a href="{{ route('show_inventory_list') }}">
-        <button>Search Inventory</button>
-      </a>
-    </div>
+    <div class="card recent">
+      <div class="tab-content">  
+        <div id="tab-recent-inventory">
+          <h3>Recent Inventories:</h3>
+          <div class="recent-items">
+            @if (count($recent_inventories) > 0)
+              <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Adicionado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($recent_inventories as $inventory)
+                        <tr>
+                            <td>{{ $inventory->name }}</td>
+                            <td>{{ $inventory->created_at->diffForHumans() }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+              </table>              
+            @else
+              <p>Nenhum inventário encontrado.</p>           
+            @endif
+          </div>
+        </div>  
 
+        <div id="tab-recent-users" style="display: none">
+          <h3>Recent Users:</h3>
+          <div class="recent-items">
+
+          </div>
+        </div>
+      </div>
+      <div class="tab-dots">
+        <span class="dot active" onclick="switch_tab(1)"></span> 
+        <span class="dot" onclick="switch_tab(2)"/>
+      </div>
+    </div>
   </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Inventory;
+use App\Models\User;
 
 require base_path('routes/inventory.php');
 require base_path('routes/login.php');
@@ -12,6 +13,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     $inventories = Inventory::all();
+    $recent_inventories = Inventory::orderBy('created_at', 'desc')->take(5)->get();
+    $users = User::all();
+    $recent_users = User::orderBy('created_at', 'desc')->take(5)->get();
 
-    return view('dashboard', compact('inventories'));    
+    return view('dashboard', compact('inventories', 'users', 'recent_inventories', 'recent_users'));    
 });
